b57e4569e41038f18cb33bb615122e7cba6a6d20
IsaiahHames <isaiah.hames14@gmail.com>
2025-10-21 20:44:27 -0400

    intial files

---
+++ enrichment/src/main/java/com/gsc/enrichment/GscEnrichmentApplication.java
@@ -0,0 +1,13 @@
+package com.gsc.enrichment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GscEnrichmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GscEnrichmentApplication.class, args);
+	}
+
+}
---
+++ enrichment/src/test/java/com/gsc/enrichment/GscEnrichmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsc.enrichment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GscEnrichmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
